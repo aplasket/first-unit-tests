3aba4fef5fb9a70987249106aad89ee18c7f28bb
Ashley Plasket <ashley.plasket@gmail.com>
2023-12-19 21:30:04 -0500

    unskip tests and write functions

--- functions-test.js
+++ functions-test.js
@@ -1,72 +1,72 @@
 // functions-test.js
 
-var assert = require('chai').assert;
-var addTwoNumbers = require('./functions.js').addTwoNumbers;
-var sayHello = require('./functions.js').sayHello;
-var buildCar = require('./functions.js').buildCar;
+const assert = require('chai').assert;
+const addTwoNumbers = require('./functions.js').addTwoNumbers;
+const sayHello = require('./functions.js').sayHello;
+const buildCar = require('./functions.js').buildCar;
 
 describe('functions', function () {
   describe('addTwoNumbers', function () {
-    it.skip('should be able to add zero to zero', function () {
+    it('should be able to add zero to zero', function () {
       // setup for test (if necessary)
 
       // execute function
-      var sum = addTwoNumbers(0, 0)
+      const sum = addTwoNumbers(0, 0)
 
       // assert what the result SHOULD be
       assert.equal(sum, 0)
     })
 
-    it.skip('should be able to add 1 to 0', function () {
+    it('should be able to add 1 to 0', function () {
       assert.equal(addTwoNumbers(1, 0), 1)
     })
 
-    it.skip('should be able to add 1 to 1', function () {
+    it('should be able to add 1 to 1', function () {
       assert.equal(addTwoNumbers(1, 1), 2)
     })
   })
 
   describe('sayHello', function () {
-    it.skip('should be able to say hello to Leta', function () {
+    it('should be able to say hello to Leta', function () {
       // setup for test (if necessary)
 
       // execute function
-      var greeting = sayHello('Leta')
+      const greeting = sayHello('Leta')
 
       // assert what the result SHOULD be
       assert.equal(greeting, 'Hi there Leta!')
     })
 
-    it.skip('should be able to say hello to Casey', function () {
+    it('should be able to say hello to Casey', function () {
       assert.equal(sayHello('Casey'), 'Hi there Casey!')
     })
 
-    it.skip('should not be able to say hello to Will', function () {
-      var differentMessage = sayHello('Will')
+    it('should not be able to say hello to Will', function () {
+      const differentMessage = sayHello('Will')
 
       assert.equal(differentMessage, 'No more testing Will!')
     })
   })
 
   describe('buildCar', function () {
-    it.skip('should return a car with a color', function () {
+    it('should return a car with a color', function () {
       // setup for test (if necessary)
-      var color = 'red'
+      const color = 'red'
 
       // execute function
-      var carDetails = buildCar(color)
+      const carDetails = buildCar(color)
 
       // assert what the result SHOULD be
       assert.equal(carDetails.color, color)
     })
 
-    it.skip('should return a car with a type', function () {
-      var carInfo = buildCar('red', 'truck')
+    it('should return a car with a type', function () {
+      const carInfo = buildCar('red', 'truck')
 
       assert.equal(carInfo.type, 'truck')
     })
 
-    it.skip('should return an empty object if there are no details passed', function () {
+    it('should return an empty object if there are no details passed', function () {
       assert.deepEqual(buildCar(), {})
     })
   })
--- functions.js
+++ functions.js
@@ -1,9 +1,32 @@
 // functions.js
-function addTwoNumbers() {
+function addTwoNumbers(num1, num2) {
+    return num1 + num2
+}
+
+function sayHello(name){
+    if (name === 'Will') {
+        return "No more testing Will!"
+    } else {
+        return `Hi there ${name}!`;
+    }
+}
+
+function buildCar(color, type) {
+    const carDetails = {};
+
+    if (color){
+        carDetails.color = color;
+    }
+
+    if (type){
+        carDetails.type = type;
+    }
+
+    return carDetails;
 }
 
 module.exports = {
     addTwoNumbers,
-    // sayHello,
-    // buildCar
+    sayHello,
+    buildCar
 }
